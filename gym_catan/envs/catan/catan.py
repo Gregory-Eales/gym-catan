9bf1b7db0f9dfb8248acac89d07b7308368ff481
Gregory Eales <gregoeales@aol.com>
2020-09-28 19:58:48 -0700

    Update catan.py
    added main play loop

--- gym_catan/envs/catan/catan.py
+++ gym_catan/envs/catan/catan.py
@@ -12,3 +12,31 @@ class Catan(object):
 		self.board = Board()
 		self.players = Players()
 
+	def play(self):
+
+
+		# populate board
+
+		# decide player order randomly
+
+		# place initial settlements / roads
+
+		# distribute final initial settlment resources
+
+		# main game loop:
+
+			# roll dice
+
+			# distribute resource cards
+
+			# turn loop:
+
+				# actions:
+				# ------------------------------
+				# make trades (bank or player)
+				# build settlement, roads, cities
+				# get development card
+				# end turn
+
+		pass
+
